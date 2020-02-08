197141763943a5d9d59ffcebb8bf8d23c0496b3b
shriharishastry <shriharishastrysmg@gmail.com>
2020-02-09 00:26:54 +0530

    refactor: resource api refactor

--- resource/custom_field.go
+++ resource/custom_field.go
@@ -1,6 +1,12 @@
 package resource
 
-import "github.com/shriharishastry/brightpearl/connector"
+import (
+	"fmt"
+	"github.com/shriharishastry/brightpearl/connector"
+	"log"
+	"net/http"
+	"net/url"
+)
 
 type CustomFieldResource struct {
 	resourceUrl string
@@ -9,5 +15,46 @@ type CustomFieldResource struct {
 
 type CustomField struct{
 	CustomFieldName string `json:"product_name"`
+}
+
+func (c *CustomFieldResource) GetAll(){
+	rsp, err := c.connection.SendRequest(http.MethodGet, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+
+func (c *CustomFieldResource) Get(){
+	rsp, err := c.connection.SendRequest(http.MethodGet, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+func (c *CustomFieldResource) Create()  {
+	rsp, err := c.connection.SendRequest(http.MethodPost, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+func (c *CustomFieldResource) Update()  {
+	rsp, err := c.connection.SendRequest(http.MethodPut, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+func (c *CustomFieldResource) Delete()  {
+	rsp, err := c.connection.SendRequest(http.MethodDelete, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
 }
 
--- resource/product_type.go
+++ resource/product_type.go
@@ -1,6 +1,12 @@
 package resource
 
-import "github.com/shriharishastry/brightpearl/connector"
+import (
+	"fmt"
+	"github.com/shriharishastry/brightpearl/connector"
+	"log"
+	"net/http"
+	"net/url"
+)
 
 type ProductTypeResource struct {
 	resourceUrl string
@@ -9,5 +15,47 @@ type ProductTypeResource struct {
 
 type ProductType struct{
 	ProductTypeName string `json:"product_type_name"`
+}
+
+
+func (c *ProductTypeResource) GetAll(){
+	rsp, err := c.connection.SendRequest(http.MethodGet, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+
+func (c *ProductTypeResource) Get(){
+	rsp, err := c.connection.SendRequest(http.MethodGet, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+func (c *ProductTypeResource) Create()  {
+	rsp, err := c.connection.SendRequest(http.MethodPost, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+func (c *ProductTypeResource) Update()  {
+	rsp, err := c.connection.SendRequest(http.MethodPut, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
+}
+
+func (c *ProductTypeResource) Delete()  {
+	rsp, err := c.connection.SendRequest(http.MethodDelete, c.resourceUrl, url.Values{}, http.Header{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(rsp)
 }
 
